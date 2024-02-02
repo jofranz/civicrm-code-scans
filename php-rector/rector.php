c56a4841e541e6a557a0c8ed7cbe4cc650fff239
Jo Franz <franz@systopia.de>
2024-02-02 22:36:28 +0100

    Update to new config layout

--- php-rector/rector.php
+++ php-rector/rector.php
@@ -2,48 +2,35 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/ext',
-    ]);
-
-
-    $rectorConfig->autoloadPaths([
-        // or full directory
-//        __DIR__ . '/civicrm-core',
-	'~/repositories/civicrm-core',
-    ]);
-
-
-
-    // register a single rule
-    $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
-    $rectorConfig->phpVersion(\Rector\Core\ValueObject\PhpVersion::PHP_73);
-
-    $rectorConfig->rules([
-        Rector\TypeDeclaration\Rector\Param\ParamTypeFromStrictTypedPropertyRector::class,
+return RectorConfig::configure()
+    ->withAutoloadPaths([
+        __DIR__ . '~/repositories/civicrm-core',
+    ])
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
         Rector\TypeDeclaration\Rector\ClassMethod\BoolReturnTypeFromStrictScalarReturnsRector::class,
         Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector::class,
         Rector\TypeDeclaration\Rector\FunctionLike\AddParamTypeSplFixedArrayRector::class,
-    ]);
-    //Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector::class,
-
-    // define sets of rules
-    $rectorConfig->sets([
+        Rector\DeadCode\Rector\PropertyProperty\RemoveNullPropertyInitializationRector::class,
+        Rector\Visibility\Rector\ClassMethod\ExplicitPublicClassMethodRector::class,
+        Rector\Visibility\Rector\ClassMethod\ChangeMethodVisibilityRector::class,
+        Rector\CodeQuality\Rector\Ternary\ArrayKeyExistsTernaryThenValueToCoalescingRector::class,
+        Rector\CodeQuality\Rector\Identical\BooleanNotIdenticalToNotIdenticalRector::class,
+    ])
+    ->withSkip([
+        Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class,
+        Rector\TypeDeclaration\Rector\ClassMethod\BoolReturnTypeFromStrictScalarReturnsRector::class,
+        Rector\DeadCode\Rector\Foreach_\RemoveUnusedForeachKeyRector::class,
+        Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector::class,
+        Rector\Php71\Rector\List_\ListToArrayDestructRector::class
+    ])
+    ->withPhpVersion(PhpVersion::PHP_74)
+    ->withSets([
+        LevelSetList::UP_TO_PHP_83,
         Rector\Set\ValueObject\SetList::DEAD_CODE,
     ]);
-
-    $rectorConfig->skip([
-#        __DIR__ . '*civix.php',
-        Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class,
-        //Rector\Php71\Rector\FuncCall\CountOnNullRector::class,
-	//Rector\TypeDeclaration\Rector\ClassMethod\BoolReturnTypeFromStrictScalarReturnsRector::class,
-	//Rector\DeadCode\Rector\Foreach_\RemoveUnusedForeachKeyRector::class,
-	//Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector::class,
-	Rector\Php71\Rector\List_\ListToArrayDestructRector::class
-    ]);
-
-};
